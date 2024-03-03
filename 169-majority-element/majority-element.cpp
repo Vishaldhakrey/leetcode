6e8801a4c4c3e601dd6662b8ad1b7131789c806a
curious.vishal <vishaldhakrey57@gmail.com>
2024-03-03 09:36:46 +0530

    Time: 22 ms (8.40%) | Memory: 27.5 MB (5.10%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count = 0;
-        int val = -1;
-        int votes = 0;
-        int n = nums.size();
-        for(int i=0; i<n; i++){
-            if(votes == 0){
-                val = nums[i];
-                votes=1;
-            }
-            else{
-                if(nums[i] == val){
-                    votes++;
-                }
-                else{
-                    votes--;
-                }
-            }
-        }
+        // int count = 0;
+        // int val = -1;
+        // int votes = 0;
+        // int n = nums.size();
+        // for(int i=0; i<n; i++){
+        //     if(votes == 0){
+        //         val = nums[i];
+        //         votes=1;
+        //     }
+        //     else{
+        //         if(nums[i] == val){
+        //             votes++;
+        //         }
+        //         else{
+        //             votes--;
+        //         }
+        //     }
+        // }
 
-        for(int i=0; i<n; i++){
-            if(nums[i] == val){
-                count++;
-            }
-        }
-        if(count>n/2){
-            return val;
-        }
-        return -1;
+        // for(int i=0; i<n; i++){
+        //     if(nums[i] == val){
+        //         count++;
+        //     }
+        // }
+        // if(count>n/2){
+        //     return val;
+        // }
+        // return -1;
+
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+        return nums[n/2];
     }
 };
