6ceb9e0d66c96aaa49d0662b269f079ed6e86e21
curious.vishal <vishaldhakrey57@gmail.com>
2024-03-03 09:46:04 +0530

    Time: 19 ms (20.45%) | Memory: 27.9 MB (5.10%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -29,10 +29,25 @@ public:
         //     return val;
         // }
         // return -1;
+/***************************************/
+        // sort(nums.begin(), nums.end());
 
-        sort(nums.begin(), nums.end());
+        // int n = nums.size();
+        // return nums[n/2];
 
+/****************************************/
         int n = nums.size();
-        return nums[n/2];
+        unordered_map<int, int>m;
+        for(int i=0; i<n; i++){
+            m[nums[i]]++;
+        }
+        n=n/2;
+
+        for(auto x : m){
+            if(x.second > n){
+                return x.first;
+            }
+        }
+        return -1;
     }
 };
