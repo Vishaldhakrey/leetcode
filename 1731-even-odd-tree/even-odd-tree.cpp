6e95405793ca0b7468d0b4eabd7e33dd9fac1d9b
curious.vishal <vishaldhakrey57@gmail.com>
2024-03-03 23:39:50 +0530

    Time: 196 ms (57.94%) | Memory: 149.2 MB (52.09%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.cpp
+++ 1731-even-odd-tree/even-odd-tree.cpp
@@ -12,40 +12,88 @@
 class Solution {
 public:
     bool isEvenOddTree(TreeNode* root) {
-    if (root == nullptr)
+    // if (root == nullptr)
+    //         return false;
+
+        // bool even = true;
+        // std::queue<TreeNode*> q;
+        // q.push(root);
+
+        // while (!q.empty()) {
+        //     auto levelSize = q.size();
+        //     auto prev = even ? std::numeric_limits<int>::min() : std::numeric_limits<int>::max();
+
+        //     for (auto i = 0; i < levelSize; i++) {
+        //         TreeNode* curr = q.front();
+        //         q.pop();
+
+        //         if (even) {
+        //             if (curr->val % 2 == 0 || curr->val <= prev)
+        //                 return false;
+        //         } else {
+        //             if (curr->val % 2 != 0 || curr->val >= prev)
+        //                 return false;
+        //         }
+
+        //         prev = curr->val;
+
+        //         if (curr->left)
+        //             q.push(curr->left);
+        //         if (curr->right)
+        //             q.push(curr->right);
+        //     }
+            
+        //     even = !even;
+        // }
+
+        if(root == nullptr)
             return false;
+            
+        std::queue<TreeNode*> queue;
+        queue.push(root);
+        int level = -1;
 
-        bool even = true;
-        std::queue<TreeNode*> q;
-        q.push(root);
+        while(!queue.empty()) {
+            level++;
+            int size = queue.size();
+            int prev = 0;
 
-        while (!q.empty()) {
-            auto levelSize = q.size();
-            auto prev = even ? std::numeric_limits<int>::min() : std::numeric_limits<int>::max();
+            for(int i = 0; i < size; i++) {
+                TreeNode* curr = queue.front();
+                queue.pop();
 
-            for (auto i = 0; i < levelSize; i++) {
-                TreeNode* curr = q.front();
-                q.pop();
+                if(level == 0 && curr->val % 2 == 0)                        
+                    return false;
 
-                if (even) {
-                    if (curr->val % 2 == 0 || curr->val <= prev)
-                        return false;
-                } else {
-                    if (curr->val % 2 != 0 || curr->val >= prev)
+                if(i == 0) {
+                    if((level % 2 == 0 && curr->val % 2 == 1) || (level % 2 == 1 && curr->val % 2 == 0))                    
+                        prev = curr->val;
+                    else
                         return false;
                 }
-
-                prev = curr->val;
-
-                if (curr->left)
-                    q.push(curr->left);
-                if (curr->right)
-                    q.push(curr->right);
+                else {                    
+                    if(level % 2 == 1) {
+                        if(curr->val % 2 == 0 && prev > curr->val)
+                            prev = curr->val;
+                        else
+                            return false;
+                    }
+                    else {
+                        if(curr->val % 2 == 1 && prev < curr->val)
+                            prev = curr->val;
+                        else
+                            return false;
+                    }
+                }
+                                        
+                if(curr->left != nullptr)
+                    queue.push(curr->left);
+                
+                if(curr->right != nullptr)
+                    queue.push(curr->right);
             }
-            
-            even = !even;
         }
-
+        
         return true;
     }
 };
