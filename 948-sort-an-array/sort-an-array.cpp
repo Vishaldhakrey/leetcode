64e4cfc407df7a6667a67d281362e3f3e6c01492
curious.vishal <vishaldhakrey57@gmail.com>
2024-03-07 22:06:07 +0530

    Time: 1893 ms (5.01%) | Memory: 71.9 MB (54.40%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int partition(std::vector<int>& nums, int low, int high) {
+        int pivotIndex = low + rand() % (high - low + 1);
+        swap(nums[low], nums[pivotIndex]);
+        int pivot = nums[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (i <= high && nums[i] <= pivot) {
+                i++;
+            }
+
+            while (j >= low && nums[j] > pivot) {
+                j--;
+            }
+            if (i < j) {
+                std::swap(nums[i], nums[j]);
+            }
+        }
+        std::swap(nums[low], nums[j]);
+        return j;
+    }
+
+    void quickSort(std::vector<int>& nums, int low, int high) {
+        while (low < high) {
+            int partIndex = partition(nums, low, high);
+            quickSort(nums, low, partIndex - 1);
+            low = partIndex + 1;
+        }
+    }
+
+    std::vector<int> sortArray(std::vector<int>& nums) {
+        quickSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+};
