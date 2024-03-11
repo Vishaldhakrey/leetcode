52320eb5260dda653819cddd2e4bd590b5dbdb2d
curious.vishal <vishaldhakrey57@gmail.com>
2024-03-11 23:25:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (62.01%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    void findPermutation(vector<int>& nums, vector<vector<int>>&ans, vector<int>&ds, int freq[]){
-        if(nums.size() == ds.size()){
-            ans.push_back(ds);
+    void findPermutation(vector<int>& nums, vector<vector<int>>&ans, int ind){
+        if(nums.size() == ind){
+            ans.push_back(nums);
             return;
         }
-        for(int i=0; i<nums.size(); i++){
-            if(!freq[i]){
-                ds.push_back(nums[i]);
-                freq[i] = 1;
-                findPermutation(nums, ans, ds, freq);
-                freq[i] = 0;
-                ds.pop_back();
-            }
+
+        for(int i=ind; i<nums.size(); i++){
+            swap(nums[ind], nums[i]);
+            findPermutation(nums, ans, ind+1);
+            swap(nums[ind], nums[i]);
         }
+        // if(nums.size() == ds.size()){
+        //     ans.push_back(ds);
+        //     return;
+        // }
+        // for(int i=0; i<nums.size(); i++){
+        //     if(!freq[i]){
+        //         ds.push_back(nums[i]);
+        //         freq[i] = 1;
+        //         findPermutation(nums, ans, ds, freq);
+        //         freq[i] = 0;
+        //         ds.pop_back();
+        //     }
+        // }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector < vector < int >> ans;
-        vector < int > ds;
-        int freq[nums.size()];
-        for (int i = 0; i < nums.size(); i++) freq[i] = 0;
-       findPermutation(nums, ans, ds, freq);
+        findPermutation(nums, ans, 0);
         return ans;
     }
 };
