338032acac7048fd1688959420ba722eabb69b94
curious.vishal <vishaldhakrey57@gmail.com>
2024-04-21 09:34:08 +0530

    Time: 18 ms (65.41%) | Memory: 24.5 MB (99.09%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -3,22 +3,23 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
         
-        if(n == 1){
-            return nums[0];
-        }
-        for(int i=0; i<nums.size(); i++){
-            if(i == 0){
-                if(nums[i]!=nums[i+1]) return nums[i];
+        if(n == 1) return nums[0];
 
-            }
-            else if(i == n-1){
-                if(nums[i] != nums[i-1]) return nums[i];
-            }
-            else{
-                if(nums[i] != nums[i+1] && nums[i] != nums[i-1]){
-                    return nums[i];
-                }
-            }
+        if(nums[0] != nums[1]) return nums[0];
+
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+
+        int l = 0, r = n-1;
+
+        while(l<=r){
+
+            int mid = (l+r)/2;
+
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+
+            if((mid%2 == 1 && nums[mid] == nums[mid-1]) || (mid%2 == 0 && nums[mid] == nums[mid+1])) l = mid+1;
+
+            else r = mid-1;
         }
         return -1;
     }
