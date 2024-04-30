19cdd8af5c7177502d8964dda4641a4882649a1f
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2024-04-30 09:59:38 +0530

    Time: 34 ms (18.94%) | Memory: 95 MB (43.28%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -2,45 +2,47 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    auto n = nums1.size();
-    auto m = nums2.size();
-    auto l = 0;
-    auto r = 0;
-    auto t = (n+m);
-    auto med1 = t/2;
-    auto med2 = t/2-1;
-    auto count = 0;
-    auto indel1 = -1;
-    auto indel2 = -1;
-    while (l < n && r < m) {
-        if (nums1[l] <= nums2[r]) {
-            if(count == med1) indel1 = nums1[l];
-            if(count == med2) indel2 = nums1[l];
-            l++;
-            count++;
-        } else {
-            if(count == med1) indel1 = nums2[r];
-            if(count == med2) indel2 = nums2[r];
-            r++;
-            count++;
+        
+        auto n = nums1.size();
+        auto m = nums2.size();
+
+        if(n == 0 && m == 0) return 0;
+
+               
+        vector<int>temp;
+
+        auto left = 0;
+        auto right = 0;
+        
+
+        while(left<n && right<m){
+            if(nums1[left] <= nums2[right]){
+                temp.push_back(nums1[left]);
+                left++;
+            }
+            else{
+                temp.push_back(nums2[right]);
+                right++;
+                
+            }
         }
-    }
-    while (l < n) {
-        if(count == med1) indel1 = nums1[l];
-        if(count == med2) indel2 = nums1[l];
-        l++;
-        count++;
-    }
-    while (r < m) {
-        if(count == med1) indel1 = nums2[r];
-        if(count == med2) indel2 = nums2[r];
-        r++;
-        count++;
-    }
-    if (t % 2 == 1) {
-        return indel1;
-    }
-    return (double)(indel1+indel2) / 2.0;
+
+        while(left<n){
+            temp.push_back(nums1[left]);
+            left++;
+        }
+        while(right<m){
+            temp.push_back(nums2[right]);
+            right++;
+        }
+        int size = temp.size();
+
+        if(size%2 == 0){
+            int ind1 = size/2;
+            int ind2 = size/2-1;
+            return (temp[ind1]+temp[ind2])/2.0;
+        }
+        return temp[size/2];
     }
 
 };
