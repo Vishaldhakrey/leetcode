02bb37636a47780b2da5c7191ee37d38974d5863
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2024-05-09 14:16:52 +0530

    Time: 4 ms (38.29%) | Memory: 9.4 MB (54.14%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        auto curr = head;
-        auto dummy = new ListNode(0) ;
-        dummy->next = head;
-        auto leftPrev = dummy;
-       
-        for(int i=0; i<left-1; i++){
-            leftPrev = curr;
+        ListNode* dummyNode = new ListNode(0);
+        dummyNode->next = head;  
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        ListNode* nextPtr = nullptr;
+        int i=0;
+
+        while (curr!=nullptr && i<left-1){
+            prev = curr;
             curr = curr->next;
+            ++i;
         }
-        ListNode* prev = nullptr; // Corrected null to nullptr
-        for(int i=0; i<right-left+1; i++){
-             ListNode* store = curr->next;
+       i=0;
+        ListNode* lastPtr_sublist = prev;
+        ListNode* firstPtr_subList = curr;
+
+        while (curr!=nullptr && i<right-left+1){
+            nextPtr = curr->next;
             curr->next = prev;
             prev = curr;
-            curr = store;
+            curr = nextPtr;
+            ++i;
         }
 
-        leftPrev->next->next = curr;
-        leftPrev->next = prev;
-
-        return dummy->next;
-
+        if(lastPtr_sublist != nullptr){
+            lastPtr_sublist->next = prev;
+        }
+        else{
+            head = prev;
+        }
+        firstPtr_subList->next = curr;
+        return head;
     }
+    
 };
