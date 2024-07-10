21104c83bd57f6f181ca2a79d19e274401c1e9d4
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2024-07-10 20:32:02 +0530

    Time: 153 ms (85.42%) | Memory: 116.6 MB (83.50%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,28 +11,57 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int>st;
-        ListNode* temp = head;
+        // vector<int>st;
+        // ListNode* temp = head;
 
-        while(temp!=NULL){
-            st.push_back(temp->val);
-            temp = temp->next;
+        // while(temp!=NULL){
+        //     st.push_back(temp->val);
+        //     temp = temp->next;
             
-        }
+        // }
 
-        int left = 0;
-        int right = st.size()-1;
+        // int left = 0;
+        // int right = st.size()-1;
         
-        while(left<=right){
-            if(st[left]==st[right]){
-                left++;
-                right--;
-                if(left>right) return true;
-            }
-            else{
-                return false;
-            }
+        // while(left<=right){
+        //     if(st[left]==st[right]){
+        //         left++;
+        //         right--;
+        //         if(left>right) return true;
+        //     }
+        //     else{
+        //         return false;
+        //     }
            
+        // }
+
+        ListNode *fast = head, *slow = head, *temp, *prev;
+
+        if(head->next == NULL) return true;
+
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+
+        }
+        prev = slow;
+        slow = slow->next;
+        prev->next = NULL;
+
+        while(slow){
+            temp = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = temp;
+        } 
+        fast = head;
+        slow = prev;
+        while(slow){
+            if(slow->val!=fast->val) return false;
+            else{
+                fast = fast->next;
+                slow = slow->next;
+            }
         }
 
         return true;
