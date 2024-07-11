a642044928f578fa8254a4c76e9eab1cdbe0121d
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2024-07-11 10:50:50 +0530

    Time: 144 ms (32.24%) | Memory: 73.4 MB (50.44%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -11,28 +11,55 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
+    
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* slow =head;
+        ListNode* fast = head;
+        ListNode* temp = NULL;
+
+        while(fast && fast->next){
+            temp = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+
+        }
+        temp->next = NULL;
+
+        ListNode* l1 = sortList(head);
+        ListNode* l2 = sortList(slow);
+
+        return mergeList(l1, l2);
+
+    }
+    ListNode* mergeList(ListNode* l1, ListNode* l2){
+        ListNode* dummy = new ListNode(0);
+
+        ListNode* curr = dummy;
+
+        while(l1 && l2){
+            if(l1->val <= l2->val){
+                curr->next = l1;
+                l1= l1->next;
+            }
+            else{
+                curr->next = l2;
+                l2 = l2->next;
+
+            }
+            curr = curr->next;
+        }
+
+         if(l1 != NULL)
+        {
+            curr -> next = l1;
+            l1 = l1->next;
+        }
         
-        vector<int>st;
-
-        if(head == nullptr || head->next ==  nullptr) return head;
-
-        ListNode* temp = head;
-
-        while(temp){
-            st.push_back(temp->val);
-            temp = temp->next;
+        if(l2 != NULL)
+        {
+            curr -> next = l2;
+            l2 = l2 ->next;
         }
-
-        sort(st.begin(), st.end());
-
-        temp = head;
-
-        for(int i=0; i<st.size(); i++){
-            temp->val = st[i];
-            
-            temp = temp->next;
-        }
-
-        return head;
+        return dummy->next;
     }
 };
