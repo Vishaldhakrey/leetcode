38ef99fdf053e1ca61d016e639c306383c370ec1
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2024-07-16 11:56:17 +0530

    Time: 2 ms (86.37%) | Memory: 12.2 MB (29.88%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,33 +2,29 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         
+        map<int, int>mp;
+        stack<int>st;
 
-        vector<int>ans;
-        int n = nums2.size();
-        for(int i=0; i<nums1.size(); i++){
-            int j=0;
-            while(j<n){
-                if(nums1[i] == nums2[j]){
-                    int k = j;
-                    int flag = 0;
-                    while(k<n){
-                        if(k+1<n && nums1[i]<nums2[k+1]){
-                         ans.push_back(nums2[k+1]);
-                         flag = 0;
-                         break;
-                        }
-                        k++;
-                        flag = 1;
-                    }
-                    if(flag == 1){
-                        ans.push_back(-1);
-                        break;
-                    }
-                  
-                }
-                j++;
+        for(int i=nums2.size()-1; i>=0; i--){
+            while(!st.empty() && st.top()<nums2[i]){
+                st.pop();
             }
+
+            if(st.empty()){
+                mp[nums2[i]] = -1;
+            }
+            else{
+                mp[nums2[i]] = st.top();
+            }
+
+            st.push(nums2[i]);
         }
+        vector<int> ans;
+        for(int i : nums1){
+            ans.push_back(mp[i]);
+        }
+
         return ans;
+       
     }
 };
