9991e8428565b34803c1f280ee569a05630fa12e
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2024-07-25 12:07:31 +0530

    Time: 2 ms (42.23%) | Memory: 10 MB (24.19%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -15,23 +15,31 @@ public:
         if(root == NULL) return {};
 
         vector<int>ans;
-        stack<TreeNode*>st1;
-        stack<TreeNode*>st2;
+        stack<TreeNode*>st;
+        TreeNode* curr = root;
 
-        st1.push(root);
+        while(!st.empty() || curr!=NULL ){
+            if(curr!=NULL){
+                st.push(curr);
+                curr = curr->left;
+            }
+            else{
+                TreeNode* temp = st.top()->right;
+                if(temp == NULL){
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
 
-        while(!st1.empty()){
-            TreeNode* temp = st1.top();
-            st1.pop();
-            st2.push(temp);
-
-            if(temp->left!=NULL) st1.push(temp->left);
-            if(temp->right!=NULL) st1.push(temp->right);
-        }
-        while(!st2.empty()){
-            root = st2.top();
-            st2.pop();
-            ans.push_back(root->val);
+                    while(!st.empty() && temp == st.top()->right){
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else{
+                    curr = temp;
+                }
+            }
         }
         return ans;
     }
