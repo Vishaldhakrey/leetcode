62dd0c10da53f45dcee7524b69872776db888e9e
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2024-08-01 19:35:29 +0530

    Time: 25 ms (49.02%) | Memory: 33.6 MB (8.96%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr, right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == NULL) return NULL;
+
+        // Handle the root node deletion
+        if (root->val == key) {
+            return helper(root);
+        }
+
+        TreeNode* dummy = root;
+        while (root != NULL) {
+            if (root->val > key) {
+                if (root->left != NULL && root->left->val == key) {
+                    root->left = helper(root->left);
+                    break;
+                } else {
+                    root = root->left;
+                }
+            } else {
+                if (root->right != NULL && root->right->val == key) {
+                    root->right = helper(root->right);
+                    break;
+                } else {
+                    root = root->right;
+                }
+            }
+        }
+        return dummy;
+    }
+
+    TreeNode* helper(TreeNode* root) {
+        if (root->left == NULL) {
+            return root->right;
+        }
+        if (root->right == NULL) {
+            return root->left;
+        }
+
+        TreeNode* rightChild = root->right;
+        TreeNode* lastRightChild = findLastRightChild(root->left);
+        lastRightChild->right = rightChild;
+        return root->left;
+    }
+
+    TreeNode* findLastRightChild(TreeNode* root) {
+        if (root->right == NULL) {
+            return root;
+        }
+        return findLastRightChild(root->right);
+    }
+};
