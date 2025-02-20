56d9255f0e8373989fc7a132b4bcb230a40deaea
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2025-02-20 20:10:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (38.12%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
-    string findDifferentBinaryString(vector<string>& nums) {
-        int n = nums.size();
-
-        string result = "";
-        for (int i=0; i<n; i++) {
-            if (nums[i][i] == '0') {
-                result += '1';
+    int n;
+    void findValidNum(string &s, unordered_set<string> &st,string & result, int ind) {
+        if (ind == n) {
+            if(st.find(s) == st.end()) {
+                result = s;
             }
-            else {
-                result += '0';
-            }
+            return;
         }
+        
+        s[ind] = '1';
+        findValidNum(s, st, result, ind+1);
+
+        if (!result.empty()) {
+            return;
+        }
+        
+        s[ind] = '0';
+        findValidNum(s, st, result, ind+1);
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        n = nums.size();
+        unordered_set<string>st;
+
+        for (auto &num : nums) {
+            st.insert(num);
+        }
+        string s(n, '0');
+        string result = "";
+        findValidNum(s, st, result, 0);
         return result;
     }
 };
