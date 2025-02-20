9ba3d182659c0a1bf8448198b7e27aff6e3220ad
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2025-02-20 21:00:45 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (29.04%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -3,15 +3,12 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
         
+        unordered_map<int, int>um;
         unordered_set<int>st;
         for(int i=0; i<n; i++) {
-            int count = 0;
-            for (int j=0; j<n; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-            if (count > n/3) {
+            um[nums[i]]++;
+            
+            if (um[nums[i]] > n/3) {
                 st.insert(nums[i]);
             }
         }
