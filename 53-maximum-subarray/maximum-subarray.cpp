eba288b1d8d575945b081e860297fb0083c63182
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2025-02-22 17:55:07 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (80.60%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        //using kadan's algorithm
         int maxSum = INT_MIN;
+
         int sum = 0;
-        for(int i=0; i<n; i++){
-            sum+=nums[i];
-            if(sum > maxSum){
+        for (int i=0; i<nums.size(); i++) {
+            sum += nums[i];
+
+            if (sum > maxSum) {
                 maxSum = sum;
-            }
-            if(sum<0){
+            } 
+            if (sum < 0) {
                 sum = 0;
             }
         }
