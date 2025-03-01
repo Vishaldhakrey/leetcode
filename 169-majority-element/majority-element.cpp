b7b58bcf85fe7e816eb759ceefb0319959872fe6
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2025-03-01 15:22:06 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (24.63%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,53 +1,23 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        // int count = 0;
-        // int val = -1;
-        // int votes = 0;
-        // int n = nums.size();
-        // for(int i=0; i<n; i++){
-        //     if(votes == 0){
-        //         val = nums[i];
-        //         votes=1;
-        //     }
-        //     else{
-        //         if(nums[i] == val){
-        //             votes++;
-        //         }
-        //         else{
-        //             votes--;
-        //         }
-        //     }
-        // }
-
-        // for(int i=0; i<n; i++){
-        //     if(nums[i] == val){
-        //         count++;
-        //     }
-        // }
-        // if(count>n/2){
-        //     return val;
-        // }
-        // return -1;
-/***************************************/
-        // sort(nums.begin(), nums.end());
-
-        // int n = nums.size();
-        // return nums[n/2];
-
-/****************************************/
-        int n = nums.size();
-        unordered_map<int, int>m;
-        for(int i=0; i<n; i++){
-            m[nums[i]]++;
-        }
-        n=n/2;
-
-        for(auto x : m){
-            if(x.second > n){
-                return x.first;
+    int majorityElement(vector<int>& arr) {
+        int n = arr.size();
+        
+        int ele = -1;
+        int count = 0;
+        for (int i=0; i<n; i++) {
+            if (count == 0) {
+                count = 1;
+                ele = arr[i];
+            }
+            else if (arr[i] == ele) {
+                count++;
+            }
+            else {
+                count--;
             }
         }
-        return -1;
+        
+        return ele;
     }
 };
