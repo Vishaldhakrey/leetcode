1ae78eed7416f21e1e8fbe85ea82418570be2064
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2025-03-06 15:20:18 +0530

    Time: 51 ms (16.89%) | Memory: 105.9 MB (29.81%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,45 +1,28 @@
 class Solution {
 public:
-    int findMaxFreq(int idx, int k, vector<int> &nums, vector<long long>& preSum) {
-        int target = nums[idx];
-
-        int i = 0; 
-        int j = idx;
-        int result = idx;
-        
-        while (i <= j) {
-            int mid = i + (j-i)/2;
-            
-            long count     = (idx - mid + 1);
-            long windowSum = (count * target);
-            long currSum   = preSum[idx] - (mid > 0 ? preSum[mid-1] : 0);
-            int opn = windowSum - currSum;
-
-            if (opn <= k) {
-                result = mid;
-                j = mid - 1;
-            } else {
-                i = mid + 1;
-            }
-
-        }
-        return idx - result + 1;
-    }
+    
     int maxFrequency(vector<int>& nums, int k) {
         int n = nums.size();
 
-        vector<long long>preSum(n, 0);
-
         sort(nums.begin(), nums.end());
-        preSum[0] = nums[0];
-
-        for (int i=1; i<n; i++) {
-            preSum[i] = preSum[i-1] + nums[i];
-        }
 
         int result = 0;
-        for (int i=0; i<n; i++) {
-            result = max(result, findMaxFreq(i, k, nums, preSum));
+        long long windowSum = 0;
+        int left = 0;
+        int right = 0;
+
+        while (right < n) {
+            long long target = nums[right];
+
+            windowSum += nums[right];
+
+            while ((target * (right - left + 1) - windowSum) > k) {
+                windowSum -= nums[left];
+                left++;
+            }
+
+            result = max(result, right-left+1);
+            right++;
         }
         return result;
     }
