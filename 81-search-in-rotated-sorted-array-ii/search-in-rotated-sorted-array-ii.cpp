a910b777bb46d37d3a769d23f7c0cd2cbc1ae460
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2025-03-21 18:14:59 +0530

    Time: 3 ms (3.38%) | Memory: 18.1 MB (20.55%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-       int l=0; 
-        int r = nums.size()-1;
-        
-        while(l<=r){
-           
-           int mid = (l+r)/2;
+        int n = nums.size();
 
-           if(nums[mid] == target){
+        int left  = 0;
+        int right = n-1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
                 return true;
-           }
-
-           if(nums[l] == nums[mid] && nums[mid] == nums[r]){
-                l = l+1;
-                r = r-1;
-                continue;
-           }
-
-           else if(nums[l] <= nums[mid]){
-                if(nums[l] <= target && target <= nums[mid]){
-                    r = mid-1;
+            }
+            //special case when arr[left] = arr[right] = arr[mid] like [1, 1, 1, 1, 1, 1, 2, 1, 1] and target = 2;
+            if (nums[mid] == nums[right] && nums[mid] == nums[left]) {
+                left++;
+                right--;
+            }
+            else if (nums[mid] <= nums[right]) {
+                if (nums[mid] < target && target <= nums[right] ) {
+                    left = mid+1;
                 }
-                else{
-                    l = mid+1;
+                else {
+                    right = mid - 1;
                 }
-           }
-           
-           else{
-                if(target >= nums[mid] &&  target <= nums[r])
-                {
-                    l = mid+1;
+            }
+            else {
+                if (nums[mid] > target && target >= nums[left]) {
+                    right = mid-1;
                 }
-                else{
-                    r = mid -1;
+                else {
+                    left = mid+1;
                 }
-           }
+            }
         }
-        return false; 
+    return false;
+
     }
 };
