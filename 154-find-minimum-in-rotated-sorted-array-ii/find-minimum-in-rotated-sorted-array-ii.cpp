be1b3d21561685b26c24cf81348fa4cb136d08ee
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2025-03-22 10:25:36 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (65.18%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -5,21 +5,32 @@ public:
 
         int left = 0;
         int right = n-1;
-        int minValue = INT_MAX;
-
+        int minEle = INT_MAX;
+        int indx = -1;
         while (left < right) {
-            int mid = left + (right-left)/2;
-
-            if (nums[mid] == nums[right]) {
-                right--;
-            }
-            else if (nums[mid] < nums[right]) {
+            int mid = left + (right -left) / 2;
+            
+            //CHECKING SORTED PART
+            if (nums[mid] < nums[right]) {
+                if (minEle > nums[mid]) {
+                    indx = mid;
+                    minEle = nums[mid];
+                }
                 right = mid;
             }
+            //UNOSERTED PART
+            else if (nums[mid] > nums[right]) {
+                if (minEle > nums[left]) {
+                    indx = mid;
+                    minEle = nums[left];
+                }
+                left = mid+1;               
+            }
             else {
-                left = mid+1;
+                right--;
             }
         }
-        return nums[left];
+        cout<<indx;
+        return min(minEle, nums[left]);
     }
 };
