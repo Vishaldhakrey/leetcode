a5940cebd3499320e756673b6032cb7cb812da35
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2025-03-27 13:05:11 +0530

    Time: 35 ms (49.85%) | Memory: 31.4 MB (71.02%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n = s.size();
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+
+        // Find LCS between original and reversed string
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s[i-1] == rev[j-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return n - dp[n][n]; // Min insertions required
+    }
+};
