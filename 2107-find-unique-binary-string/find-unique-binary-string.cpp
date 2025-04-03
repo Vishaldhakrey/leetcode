98df6b8c24bd5f3699d9f641a90bdea1c29e2d37
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2025-04-03 21:57:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (82.99%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -4,19 +4,12 @@ public:
     string findDifferentBinaryString(vector<string>& nums) {
         int n = nums.size();
 
-        unordered_set<int>st;
-        for (auto &num : nums) {
-            st.insert(stoi(num, 0, 2));
-        }
-
         string result = "";
 
-        for (int i=0; i<=n; i++) {
-            if (st.find(i) == st.end()) {
-                result = bitset<16>(i).to_string();
-            }
+        for (int i=0; i<n; i++) {
+            char ch = (nums[i][i] == '0') ? '1' : '0';
+            result += ch;
         }
-
-        return result.substr(16-n, n);
+        return result;
     }
 };
