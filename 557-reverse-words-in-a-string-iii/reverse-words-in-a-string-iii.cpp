16686622afff0aff0d00317246b66440a870a665
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2025-04-03 09:31:34 +0530

    Time: 5 ms (33.93%) | Memory: 14.9 MB (40.35%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n = s.size();
-        for (int i=0; i<n; i++) {
-            if (s[i] != ' ') {
-                int j = i;
-                while (j < n && s[j] != ' ') {
-                    j++;
-                }
-                reverse(s.begin()+i, s.begin()+j);
-                i = j;
-            }
-        }
-        return s;
+        stringstream ss(s);
+
+        string token = "";
+        s = "";
+
+        while (ss >> token) {
+            reverse(token.begin(), token.end());
+            s += token + " ";
+        }        
+        return s.substr(0, s.size()-1);
     }
 
 };
