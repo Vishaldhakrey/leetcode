6ac116f88e1aba8292422aa0919dbfd872b8adaa
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2025-04-08 17:13:16 +0530

    Time: 9 ms (59.13%) | Memory: 10.9 MB (66.86%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
+//TimeComplexity of this is O(n3)
     int maximumLength(string s) {
         int n = s.size();
 
-        unordered_map<string, int>mp;
+        map<pair<char, int>, int>mp;
 
-        for (int i=0; i<n; i++) {
-            string curr;
-            for (int j=i; j<n; j++) {
+        for (int i=0; i<n; i++) { //O(n)
+            int len = 0;
+            for (int j=i; j<n; j++) { //O(n)
                 if (s[j] == s[i]) {
-                    curr += s[j];
-                    mp[curr]++;
+                    len += 1;
+                    mp[{s[j], len}]++; //To copy string O(n)
                 }
                 else {
                     break;
                 }
             }
         }
-
-        string result = "";
+        int result = 0;
         for (auto &it : mp) {
-            string str = it.first;
+            int str_size = it.first.second;
             int cnt = it.second;
-            if (cnt >= 3 && str.size() > result.size()) {
-                result = str;
+            if (cnt >= 3 && str_size > result) {
+                result = str_size;
             }
         }
 
-        return !result.empty() ? result.size() : -1;
+        return result != 0 ? result : -1;
     }
 };
