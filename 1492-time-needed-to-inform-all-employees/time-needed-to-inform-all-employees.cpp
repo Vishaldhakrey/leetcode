c0ea8c308f5d2d218981b5f59f8633aee0f07eaa
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2025-10-07 07:04:08 +0530

    Time: 235 ms (20.83%) | Memory: 161.7 MB (15.54%) - LeetSync

---
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxTime = INT_MIN;
+    void DFS(unordered_map<int, vector<int>>&adj, vector<int>& informTime, int curr_employee, int curr_time) {
+        maxTime = max(maxTime, curr_time);
+
+        for (int &employee : adj[curr_employee]) {
+
+            DFS(adj, informTime, employee, curr_time + informTime[curr_employee]);
+
+        }
+    }
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        unordered_map<int, vector<int>>adj;
+
+        for (int i=0; i<manager.size(); i++) {
+            int employee_i = i;
+            int manager_i = manager[i];
+
+            if (manager_i != -1) {
+                adj[manager_i].push_back(employee_i);
+            }
+            
+        }
+        DFS(adj, informTime, headID, 0);
+        return maxTime;
+
+    }
+};
