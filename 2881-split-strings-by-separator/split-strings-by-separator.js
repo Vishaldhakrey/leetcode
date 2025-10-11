e527968172a64c32b8fddf52117b72e3bba65960
Vishal Dhakrey <vishaldhakrey57@gmail.com>
2025-10-11 07:46:44 +0530

    Time: 10 ms (17.18%) | Memory: 63.1 MB (22.91%) - LeetSync

--- 2881-split-strings-by-separator/split-strings-by-separator.js
+++ 2881-split-strings-by-separator/split-strings-by-separator.js
@@ -4,15 +4,5 @@
  * @return {string[]}
  */
 var splitWordsBySeparator = function(words, separator) {
-    let result = [];
-    for (let word of words) {
-        let temp = word.split(separator);
-        for(let sep_word of temp) {
-            if(sep_word.length > 0) {
-            result.push(sep_word);
-
-            }
-        }
-    }
-    return result;
+    return words.flatMap(word => word.split(separator).filter(sep_word => sep_word.length > 0));
 };
